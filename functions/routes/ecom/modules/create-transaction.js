d6fcb7074696e8d22e7e85755d277a1af8aba25a
Leonardo Matos <leomp120894@gmail.com>
2021-02-19 04:43:42 -0300

    feat(infinitepay-callback): setup callback url and handle webhooks

--- functions/routes/ecom/modules/create-transaction.js
+++ functions/routes/ecom/modules/create-transaction.js
@@ -1,3 +1,6 @@
+const { baseUri } = require('../../../__env')
+const axios = require('axios')
+
 exports.post = ({ appSdk }, req, res) => {
   const { params, application } = req.body
   const { storeId } = req
@@ -27,4 +30,39 @@ exports.post = ({ appSdk }, req, res) => {
     redirect_to_payment: true,
     transaction
   })
+
+  // https://gist.github.com/luisbebop/ca87e04da04bcf662f732b1b6848d6ca#integration-
+  // https://infinitepay.io/docs#listar-wallets
+  const infinitepayAxiosConfig = {
+    headers: {
+      Authorization: config.infinitepay_api_key
+    }
+  }
+  const callbackUrl = `${baseUri}/infinitepay/callback`
+  axios.get('https://api.infinitepay.io/v1/wallets', infinitepayAxiosConfig)
+
+    .then(({ data }) => {
+      const { results } = data
+      const merchantWallets = results.filter(({ role }) => role === 'merchant')
+      if (!merchantWallets.find(wallet => wallet.callback_url === callbackUrl)) {
+        const endpoint = `https://api.infinitepay.io/v1/wallets/${merchantWallets[0].wallet_id}`
+        const data = {
+          callback_url: callbackUrl
+        }
+        return axios.patch(endpoint, data, infinitepayAxiosConfig)
+      }
+    })
+
+    .catch(error => {
+      const { response, config } = error
+      if (response) {
+        const { status, data } = response
+        const err = new Error(`Failed trying to setup #${storeId} InfinitePay callbacks (${status})`)
+        err.url = config && config.url
+        err.status = status
+        err.response = JSON.stringify(data)
+        return console.error(err)
+      }
+      console.error(error)
+    })
 }
---
+++ functions/routes/infinitepay/callback.js
@@ -0,0 +1,105 @@
+const axios = require('axios')
+const getAppData = require('../../lib/store-api/get-app-data')
+
+exports.post = ({ appSdk }, req, res) => {
+  // https://gist.github.com/luisbebop/ca87e04da04bcf662f732b1b6848d6ca#integration-
+  const transactionId = req.body && req.body.transaction_id
+  if (transactionId) {
+    const { metadata } = req.body
+    if (metadata && /[\d]+::[a-f0-9]+/.test(metadata)) {
+      let [storeId, orderId] = metadata.split('::')
+      storeId = parseInt(storeId, 10)
+
+      if (storeId > 100) {
+        console.log('> Callback #', storeId, orderId)
+        let infinitepayTransaction, order
+        // read configured E-Com Plus app data
+        return getAppData({ appSdk, storeId })
+          .then(config => {
+            // double check InfinitePay transaction
+            // https://infinitepay.io/docs#mostrar-transacao
+            return axios.get(`https://api.infinitepay.io/v1/transactions/${transactionId}`, {
+              headers: {
+                Authorization: config.infinitepay_api_key
+              }
+            }).then(({ data }) => {
+              infinitepayTransaction = data
+              return config
+            })
+          })
+
+          .then(config => {
+            // get E-Com Plus order
+            const resource = `orders/${orderId}.json`
+            return appSdk
+              .apiRequest(storeId, resource)
+              .then(({ response }) => {
+                order = response.data
+                return config
+              })
+          })
+
+          .then(() => {
+            // add new transaction status to payment history
+            const transaction = order.transactions.find(({ intermediator }) => {
+              return intermediator && intermediator.transaction_id === String(transactionId)
+            })
+            const resource = `orders/${order._id}/payments_history.json`
+            const method = 'POST'
+            const body = {
+              date_time: new Date().toISOString(),
+              status: parseStatus(infinitepayTransaction.status),
+              flags: ['infinitepay']
+            }
+            if (transaction) {
+              body.transaction_id = transaction._id
+            }
+            return appSdk.apiRequest(storeId, resource, method, body)
+          })
+
+          .then(() => {
+            res.sendStatus(200)
+          })
+          .catch(error => {
+            const { response, config } = error
+            if (response) {
+              const { status, data } = response
+              const err = new Error(`#${storeId} InfinitePay callback error ${status}`)
+              err.url = config && config.url
+              err.status = status
+              err.response = JSON.stringify(data)
+              return console.error(err)
+            }
+            console.error(error)
+          })
+      }
+    }
+  }
+
+  res.sendStatus(410)
+}
+
+const parseStatus = infinitepayStatus => {
+  switch (infinitepayStatus) {
+    case 'approved':
+      return 'paid'
+    case 'processing':
+    case 'analyzing':
+      return 'under_analysis'
+    case 'authorized':
+    case 'paid':
+    case 'refunded':
+      return infinitepayStatus
+    case 'waiting_payment':
+      return 'pending'
+    case 'pending_refund':
+      return 'in_dispute'
+    case 'refused':
+      return 'unauthorized'
+    case 'chargedback':
+      return 'refunded'
+    case 'pending_review':
+      return 'authorized'
+  }
+  return 'unknown'
+}
