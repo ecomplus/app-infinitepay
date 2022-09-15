bbe1e646903aed78766fb0e92fde204b09e6271a
Leonardo Matos <leomp120894@gmail.com>
2022-09-15 19:07:51 -0300

    chore: add `hostingUri` to env object [skip ci]

--- functions/__env.js
+++ functions/__env.js
@@ -1,12 +1,14 @@
 // setup server and app options from Functions config (and mocks)
 const { pkg, server } = require('firebase-functions').config()
 const functionName = server.functionName || 'app'
+const projectId = process.env.GCLOUD_PROJECT
 
 module.exports = {
   functionName,
   operatorToken: server && server.operator_token,
   baseUri: (server && server.base_uri) ||
-    `https://us-central1-${process.env.GCLOUD_PROJECT}.cloudfunctions.net/${functionName}`,
+    `https://us-central1-${projectId}.cloudfunctions.net/${functionName}`,
+  hostingUri: `https://${projectId}.web.app`,
   pkg: {
     ...pkg
   }
