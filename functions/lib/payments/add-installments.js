e2adbf58a5f3e5378a0d00327fa1203aec109d6a
Wisley Alves <wisley7lagoas@yahoo.com.br>
2023-03-16 16:12:41 -0300

    fix(add-installments): set minimum amount to not apply interest

--- functions/lib/payments/add-installments.js
+++ functions/lib/payments/add-installments.js
@@ -18,21 +18,23 @@ module.exports = (amount, installments = {}, gateway = {}, response) => {
       }
     }
 
+    const isInterestFreeMinAmount = installments.interest_free_min_amount ? amount.total >= (installments.interest_free_min_amount) : 5
+
     // list installment options
     gateway.installment_options = []
     for (let number = 2; number <= maxInstallments; number++) {
       const tax = !(maxInterestFree >= number)
       let interest
-      if (tax) {
+      if (tax || !isInterestFreeMinAmount) {
         const IPMonthInterestRate = IPInterestMonthly[number - 1]
         interest = (monthlyInterest > IPMonthInterestRate ? monthlyInterest : IPMonthInterestRate) / 100
       }
-      const value = !tax ? (amount.total / number) : amount.total * (interest / (1 - Math.pow(1 + interest, -number)))
+      const value = !tax && isInterestFreeMinAmount ? (amount.total / number) : (amount.total * (interest / (1 - Math.pow(1 + interest, -number))))
       if (value && value >= 1) {
         gateway.installment_options.push({
           number,
           value,
-          tax
+          tax: tax || !isInterestFreeMinAmount
         })
       }
     }
